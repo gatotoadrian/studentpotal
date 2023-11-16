68c2651ec5d346b65e9d8be46c26c88a7ab04f8c
Shaban <Adrianeddie8113>
2023-11-16 16:44:59 +0300

    commit message

--- StudentPotal.java
+++ StudentPotal.java
@@ -2,12 +2,12 @@ public class StudentPotal{
 public static void main(String[]args){
 System.out.println("Institution:KCA University");
 System.out.println("Course:DIT");
-System.out.println("Name:Mat Othiambo Kimani");
-System.out.println("Reg no:22/05004");
+System.out.println("Name:Njuguna Kiambuthi Kimani");
+System.out.println("Reg no:22/05054");
 System.out.println("Gender:Male");
-System.out.println("DOB:02.02.2005");
-System.out.println("Phone no:+254798745632211");
+System.out.println("DOB:02.02.2008");
+System.out.println("Phone no:+254789512567");
 
-System.out.println("Email Address:matothiambo648@gmail.com");
+System.out.println("Email Address:kiambuthinjuguna685gmail.com");
 }
 }
