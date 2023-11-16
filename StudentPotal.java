309aee05d4d1a63cec4f76db73d9698bcf00ecb8
Shaban <Adrianeddie8113>
2023-11-16 14:55:32 +0300

    initial commite

---
+++ StudentPotal.java
@@ -0,0 +1,12 @@
+public class StudentPotal{
+public static void main(String[]args){
+System.out.println("Institution:KCA University");
+System.out.println("Course:DIT");
+System.out.println("Name:Mat Othiambo Kimani");
+System.out.println("Reg no:22/05004");
+System.out.println("Gender:Male");
+System.out.println("DOB:02.02.2005");
+System.out.println("Phone no:+254798745632211");
+System.out.println("Email Address:matothiambo648@gmail.com");
+}
+}
